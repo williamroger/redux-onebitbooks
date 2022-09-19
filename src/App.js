6bcef87f6e63da4c9c4517ba9aafa3a2f56a28fe
William Roger <williamroger.frontend@gmail.com>
2022-09-19 17:49:16 -0300

    adicionado componente Header

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
